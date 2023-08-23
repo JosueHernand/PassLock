d413d7e699b7fb9060c4f28a941859aeda017428
Josue Hernandez <joseh1337@gmail.com>
2023-08-22 23:27:40 -0700

    made a prompt come up when you click on the button

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +8,14 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters):"));
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return "";
+  }
+
+}
